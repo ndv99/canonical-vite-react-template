636d903c6596b6fd6d016759b26a2ecbed9ef35e
Nick De Villiers <nick.devilliers@canonical.com>
2023-04-05 15:40:03 +0100

    Fix routes test

--- src/routes.test.tsx
+++ src/routes.test.tsx
@@ -8,7 +8,7 @@ describe("router", () => {
     it(`displays correct document title for ${title} page`, async () => {
       const router = createMemoryRouter(routes, { initialEntries: [path], initialIndex: 0 });
       render(<RouterProvider router={router} />);
-      expect(document.title).toBe(`${title} | MAAS Site Manager`);
+      expect(document.title).toBe(`${title} | Vite React Template`);
     });
   });
 });
