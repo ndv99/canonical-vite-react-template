08894b6151761bed7fa813adf8118a6136314493
Nick De Villiers <nick.devilliers@canonical.com>
2023-04-05 14:48:20 +0100

    Refactor App to use react-router-dom

---
+++ src/App.test.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable testing-library/no-node-access */
+/* eslint-disable testing-library/no-container */
+import { waitFor, render, within } from "@testing-library/react";
+
+import App from "./App";
+
+describe("App", () => {
+  it("renders vanilla layout components correctly", async () => {
+    const { container } = render(<App />);
+
+    await waitFor(() => expect(container.querySelector(".l-application")).toBeInTheDocument());
+
+    const application = container.querySelector(".l-application") as HTMLElement;
+
+    expect(application.querySelector(".l-main")).toBeInTheDocument();
+
+    expect(within(container).getByRole("heading", { name: /Canonical Vite React Template/ })).toBeInTheDocument();
+  });
+});
