b432dc1004933284db109197ceb26ee9bce30bd7
Nick De Villiers <nick.devilliers@canonical.com>
2023-04-05 14:48:48 +0100

    Create test file for main

---
+++ src/main.test.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable testing-library/no-node-access */
+import { waitFor } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+
+describe("main", () => {
+  beforeAll(() => {
+    const rootElement = document.createElement("div");
+    rootElement.setAttribute("id", "root");
+    document.body.appendChild(rootElement);
+  });
+
+  it("renders the app in the root element", async () => {
+    await act(() => import("./main"));
+    const container = document.getElementById("root") as HTMLElement;
+    await waitFor(() => expect(container.querySelector(".l-application")).toBeInTheDocument());
+  });
+});
