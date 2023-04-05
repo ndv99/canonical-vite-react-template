d976caa02c3ec6f3a7f31948490c099510fa7da6
Nick De Villiers <nick.devilliers@canonical.com>
2023-04-05 11:17:35 +0100

    Add coverage settings to vitest.config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { configDefaults, defineConfig } from "vitest/config";
+import { configDefaults, coverageConfigDefaults, defineConfig } from "vitest/config";
 import * as path from "path";
 
 export default defineConfig({
@@ -10,6 +10,18 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: ["./setupTests.ts"],
     exclude: [...configDefaults.exclude, "**/tests/**"],
+    coverage: {
+      // exclude index files as they're only used to export other files
+      // exclude pages as they're covered by playwright tests
+      exclude: [...coverageConfigDefaults.exclude, "**/index.ts", "src/mocks/**/*", "src/pages/**/*", "**/types.ts"],
+      include: ["src/**/*.{ts,tsx}"],
+      reporter: ["text", "json", "html"],
+      all: true,
+      lines: 80,
+      functions: 80,
+      branches: 80,
+      statements: 80,
+    },
     clearMocks: true,
   },
 });
